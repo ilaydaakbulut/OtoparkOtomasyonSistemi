6513c9471d8d3e9be28044421adfc98d1ea97e7a
ilaydaakbulut <akbulut.ilayda@yahoo.com.tr>
2019-12-11 15:29:48 +0300

    created django and database

---
+++ evn/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ilaydaakbulut\desktop\otoparkotomasyonsistemi\evn\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
